584b4cc1de4b4248276416331e3938c3abd8bf44
Xiangdong Lu <lxdhzzj@gmail.com>
2023-07-18 18:31:23 +0800

    init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,213 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import React, { useState } from 'react';
+
+const App = () => {
+  // State to control the visibility of the submenu and detailed submenu
+  const [showSubMenu, setShowSubMenu] = useState(false);
+  const [showDetailedSubMenu, setShowDetailedSubMenu] = useState(false);
+
+  // Sample data for menu and submenu items
+  const menuItems = [
+    {
+      label: '分类',
+      subItems: ['美食', '家装', '广告', 'UI设计', '时装'],
+    },
+    {
+      label: '最近使用',
+      subItems: ['美食商业摄影', '吉卜力动漫'],
+    },
+  ];
+
+  const images = [
+    { id: 1, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 2, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 3, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 4, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 5, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 6, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 7, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    { id: 8, imageUrl: 'https://via.placeholder.com/200x120', altText: '图片展示内容' },
+    // ... Add more image data here
+  ];
+
+  // Sample data for detailed submenu
+  const detailedSubMenuItems = ['牛排', '中餐', '日料', '法餐'];
+
+  const handleSubMenuToggle = () => {
+    setShowSubMenu(!showSubMenu);
+  };
+
+  const handleDetailedSubMenuToggle = () => {
+    setShowDetailedSubMenu(!showDetailedSubMenu);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      {/* Top Area */}
+      <div className="bg-gray-200 p-4 flex justify-between items-center">
+        <div className="flex items-center w-4/5">
+          {/* Logo */}
+          <div className="bg-blue-500 h-8 w-8"></div>
+          {/* Search box */}
+          <input
+            type="text"
+            placeholder="Search..."
+            className="ml-4 p-2 rounded-md w-4/5"
+          />
+        </div>
+
+        {/* Menu */}
+        <div className="flex items-center">
+          {/* Three menu items */}
+          <div className="mr-4">
+            <button className="px-2 py-1 rounded-md bg-blue-500 text-white">
+              文档
+            </button>
+          </div>
+          <div className="mr-4">
+            <button className="px-2 py-1 rounded-md bg-blue-500 text-white">
+              联系
+            </button>
+          </div>
+          <div className="relative mr-4">
+            <button
+              className="px-2 py-1 rounded-md bg-blue-500 text-white"
+              onMouseEnter={handleSubMenuToggle}
+              onMouseLeave={handleSubMenuToggle}
+            >
+              用户中心
+            </button>
+            {/* Submenu */}
+            {showSubMenu && (
+              <div className="absolute mt-2 bg-white border border-gray-300 p-2 rounded-md">
+                {menuItems.map((menuItem, index) => (
+                  <div key={index}>
+                    <div
+                      className="font-semibold mb-1 cursor-pointer"
+                      onMouseEnter={handleDetailedSubMenuToggle}
+                      onMouseLeave={handleDetailedSubMenuToggle}
+                    >
+                      {menuItem.label}
+                    </div>
+                    {/* Detailed Submenu */}
+                    {showDetailedSubMenu &&
+                      menuItem.label === '分类' && (
+                        <div className="ml-2">
+                          {detailedSubMenuItems.map((item, index) => (
+                            <div key={index}>
+                              <a href="#">{item}</a>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {/* Main Area */}
+      <div className="flex p-4">
+        {/* Left Menu Area */}
+        <div className="bg-gray-300 p-4 w-1/12 h-full">
+          {/* Left Menu content */}
+          <div className="mb-4">
+            <div className="font-semibold mb-2">分类</div>
+            <ul>
+              {menuItems[0].subItems.map((item, index) => (
+                <li key={index}>
+                  {/* Submenu item */}
+                  <a href="#">{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <div className="font-semibold mb-2">最近使用</div>
+            <ul>
+              {menuItems[1].subItems.map((item, index) => (
+                <li key={index}>
+                  {/* Submenu item */}
+                  <a href="#">{item}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* Right Workspace Area */}
+        <div className="flex-1 p-4 ml-4 bg-gray-300">
+          {/* Input Area */}
+          <div className="flex items-center mb-4">
+            <div className="font-semibold mr-4">Prompt</div>
+            <textarea className="flex-1 border border-gray-300 p-2"></textarea>
+            <div className="ml-4 flex-col">
+              <button className="px-2 py-1 rounded-md bg-blue-500 text-white mb-2 mr-2">
+                Copy
+              </button>
+              <button className="px-2 py-1 rounded-md bg-blue-500 text-white">
+                Save
+              </button>
+            </div>
+          </div>
+
+          {/* Filter Area */}
+          <div className="flex items-center mb-4">
+            {/* Filter inputs */}
+            <div className="flex">
+      {/* First Input */}
+      <div className="flex mr-2">
+        <label htmlFor="inputA" className="block mb-1">
+          a:
+        </label>
+        <input
+          type="text"
+          id="inputA"
+          className="border border-gray-300 rounded-md p-2"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Second Input */}
+      <div className='flex mr-2'>
+        <label htmlFor="inputB" className="block mb-1">
+          b:
+        </label>
+        <input
+          type="text"
+          id="inputB"
+          className="border border-gray-300 rounded-md p-2"
+        />
       </div>
-    </main>
-  )
-}
+    </div>
+            <button className="ml-4 px-2 py-1 rounded-md bg-blue-500 text-white">
+              过滤
+            </button>
+          </div>
+
+          {/* Item Display Area */}
+          <div className="grid grid-cols-4 gap-4">
+      {images.map((image) => (
+        <div key={image.id}>
+          {/* Image */}
+          <img src={image.imageUrl} alt={image.altText}/>
+          {/* Text */}
+          <div className="text-center">{image.altText}</div>
+        </div>
+      ))}
+    </div>
+
+          {/* Pagination */}
+          <div className="flex justify-center mt-2">
+            {/* Pagination component */}
+            {/* (Your pagination component here) */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
